390a1797340b290324de508cf1061f1318d35859
Rodion Matvieiev <matveev.r.v@icloud.com>
2024-05-02 21:11:36 -0400

    upd

--- src/generics/4.ts
+++ src/generics/4.ts
@@ -5,12 +5,22 @@ type User123 = {
   password: string;
 };
 
-function createOrUpdateUser<T extends Partial<User123>>(initialValues: T): T {
-  return { ...initialValues };
-  // Оновлення користувача
-}
-
-createOrUpdateUser({
+const user: User123 = {
+  name: "Firstname",
+  surname: "Lastname",
+  email: "firstname@gmail.com",
+  password: "lastname123",
+};
+const uptadeUser: Partial<User123> = {
   email: "user@mail.com",
   password: "password123",
-});
+};
+
+function createOrUpdateUser<T extends Partial<User123>>(
+  user: User123,
+  initialValues: T
+): User123 {
+  return { ...user, ...initialValues };
+}
+
+createOrUpdateUser(user, uptadeUser);
